4d7bebae5e785253e8d84b203c0bba27e83542a1
harshpandya43 <pandyaharsh43@gmail.com>
2026-04-14 17:51:13 -0700

    feat: initial project setup with Spring Boot 3.5, Java 21, Docker Compose

---
+++ src/main/java/com/shodh/ShodhApplication.java
@@ -0,0 +1,13 @@
+package com.shodh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShodhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShodhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shodh/ShodhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shodh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShodhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
